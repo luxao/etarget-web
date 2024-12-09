ef1f33c8018d363b59053ed568e211fcd811bd85
luxao <lukas.lobll@gmail.com>
2024-12-09 16:03:48 +0100

    Add x icon, prepare for mobile menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
-window.addEventListener("resize", function(e){
-    console.log(window.innerWidth)
-})
+//TODO: OPEN MENU
+function openMenu() {
+    e$("#menu-icon").hide()
+    e$("#x-icon").show()
+    // e$(".navigation-menu").addClass("show-menu")
+}
+
+//TODO: CLOSE MENU
+function closeMenu() {
+    // TODO:
+    //CODE HERE
+    e$("#menu-icon").show()
+    e$("#x-icon").hide()
+}
